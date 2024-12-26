4b2e77127692c589843d7032574a41e2308672a8
march <106459595+marchc1@users.noreply.github.com>
2024-12-25 16:35:52 -0800

    Move HideText to its own class file

---
+++ lua/ponder/instructions_cl/ponder.hidetext.lua
@@ -0,0 +1,23 @@
+local HideText = Ponder.API.NewInstruction("HideText")
+HideText.Name = ""
+HideText.Length = 0.5
+
+function HideText:First(playback)
+    local env = playback.Environment
+    local txt = env:GetNamedText(self.Name)
+    if not txt then return end
+end
+
+function HideText:Render(playback)
+    local env = playback.Environment
+    local object = env:GetNamedText(self.Name)
+    if not object then return end
+
+    local progress = math.ease.OutQuad(playback:GetInstructionProgress(self))
+    object.Alpha = (1 - progress) * 255
+end
+
+function HideText:Last(playback)
+    local env = playback.Environment
+    env:RemoveTextByName(self.Name)
+end
