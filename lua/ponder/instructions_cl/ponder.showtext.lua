73cf076d513ee7a31b5a6c4c2dde0b12e21335a6
march <106459595+marchc1@users.noreply.github.com>
2024-12-25 16:34:41 -0800

    Rename to ponder.showtext.lua

--- lua/ponder/instructions_cl/ponder.showtext.lua
+++ lua/ponder/instructions_cl/ponder.showtext.lua
@@ -18,6 +18,12 @@ function ShowText:First(playback)
     txt.Dimension = self.Dimension
     txt.Position = self.Position
 
+    if self.ParentTo then
+        local parent = env:GetNamedModel(self.ParentTo)
+        txt.Parent = parent
+    end
+
+
     txt:SetMarkup(self.Markup)
 end
 
@@ -28,28 +34,4 @@ function ShowText:Render(playback)
 
     local progress = math.ease.OutQuad(playback:GetInstructionProgress(self))
     object.Alpha = progress * 255
-end
-
-local HideText = Ponder.API.NewInstruction("HideText")
-HideText.Name = ""
-HideText.Length = 0.5
-
-function HideText:First(playback)
-    local env = playback.Environment
-    local txt = env:GetNamedText(self.Name)
-    if not txt then return end
-end
-
-function HideText:Render(playback)
-    local env = playback.Environment
-    local object = env:GetNamedText(self.Name)
-    if not object then return end
-
-    local progress = math.ease.OutQuad(playback:GetInstructionProgress(self))
-    object.Alpha = (1 - progress) * 255
-end
-
-function HideText:Last(playback)
-    local env = playback.Environment
-    env:RemoveTextByName(self.Name)
 end
