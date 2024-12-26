ce59a49bc31dd260c01d2a0c68aa6b1507743176
march <106459595+marchc1@users.noreply.github.com>
2024-12-26 15:09:39 -0800

    Update ponder_loader.lua

--- lua/autorun/ponder_loader.lua
+++ lua/autorun/ponder_loader.lua
@@ -2,3 +2,9 @@ include("includes/gloader.lua")
 gloader.Load("Ponder", "ponder")
 
 Ponder.Debug = true
+if SERVER then
+    -- resource.AddFile("materials/ponder/ui/toolgun_cursor64.png")
+    resource.AddFile("materials/ponder/ui/toolgun_cursor96.png")
+    -- resource.AddFile("materials/ponder/ui/toolgun_cursor128.png")
+    -- resource.AddFile("materials/ponder/ui/toolgun_cursor192.png")
+end
