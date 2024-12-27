ed96d22185debf5083531445a079de1760e3f075
march <106459595+marchc1@users.noreply.github.com>
2024-12-27 13:15:25 -0800

    Add resource

--- lua/autorun/ponder_loader.lua
+++ lua/autorun/ponder_loader.lua
@@ -12,6 +12,7 @@ if SERVER then
     resource.AddFile("materials/ponder/ui/icon64/replay.png")
     resource.AddFile("materials/ponder/ui/icon64/fast.png")
     resource.AddFile("materials/ponder/ui/icon64/magnifier.png")
+    resource.AddFile("materials/ponder/ui/icon64/magnifier_enabled.png")
     resource.AddFile("materials/ponder/ui/icon64/magnifier_no_back.png")
     resource.AddFile("materials/ponder/ui/progress/bar.png")
     resource.AddFile("materials/ponder/ui/progress/grabby.png")
