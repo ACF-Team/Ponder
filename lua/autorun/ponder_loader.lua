dc859893a6a1747ba1f65334251250d7003b2fee
march <106459595+marchc1@users.noreply.github.com>
2024-12-26 17:52:42 -0800

    Ghosteh icons

--- lua/autorun/ponder_loader.lua
+++ lua/autorun/ponder_loader.lua
@@ -7,4 +7,6 @@ if SERVER then
     resource.AddFile("materials/ponder/ui/toolgun_cursor96.png")
     -- resource.AddFile("materials/ponder/ui/toolgun_cursor128.png")
     -- resource.AddFile("materials/ponder/ui/toolgun_cursor192.png")
+    resource.AddFile("materials/ponder/ui/icon16/play.png")
+    resource.AddFile("materials/ponder/ui/icon16/stop.png")
 end
