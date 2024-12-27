2efd3e316d521c028f47fa931450ec89e4094a07
march <106459595+marchc1@users.noreply.github.com>
2024-12-27 15:42:19 -0800

    Add the new toolgun instructions + renderer

--- lua/ponder/instructions_cl/ponder.toolfromto.lua
+++ lua/ponder/instructions_cl/ponder.toolfromto.lua
@@ -1,45 +1,162 @@
-local ToolFromTo = Ponder.API.NewInstruction("ToolFromTo")
+-- TODO: Separate all of these into individual files. 
+-- ToolFromTo would be replaced by this kind of macro:
+--[[
+    ShowToolgun
+    MoveToolgunTo
+    Delay
+    ClickToolgun
+    MoveToolgunTo
+    Delay
+    ClickToolgun
+    HideToolgun
+]]
 
-ToolFromTo.Length = 2
 
 local toolsound = "Airboat.FireGunRevDown"
 local toolgun96 = Material("ponder/ui/toolgun_cursor96.png")
-function ToolFromTo:First(playback)
-    playback.ToolFromToState = 0
+
+------------------------------------------------------------------
+-- ShowToolgun
+------------------------------------------------------------------
+
+local ShowToolgun = Ponder.API.NewInstruction("ShowToolgun")
+ShowToolgun.Length = 0.5
+
+function ShowToolgun:First(playback)
+    local env = playback.Environment
+
+    env.ToolgunState = {
+        Position = Vector(0, 0, 0),
+        ToolName = self.Tool or "",
+        Alpha = 0,
+        IsShowing = true
+    }
 end
 
-function ToolFromTo:Update(playback)
-    local progress = playback:GetInstructionProgress(self)
-    local alpha = (math.Clamp(progress, 0, 0.1) / 0.1) * (math.Clamp(math.Remap(progress, 0.9, 1, 1, 0), 0, 1))
+function ShowToolgun:Update(playback)
+    local env = playback.Environment
+    local state = env.ToolgunState
+    state.Alpha = Lerp(playback:GetInstructionProgress(self), 0, 1)
+end
 
-    local movement_state = math.Clamp(math.Remap(progress, 0.25, 0.75, 0, 1), 0, 1)
-    movement_state = math.ease.InOutQuad(movement_state)
+------------------------------------------------------------------
+-- HideToolgun
+------------------------------------------------------------------
 
-    if movement_state > 0 and playback.ToolFromToState == 0 then
-        playback.ToolFromToState = 1
-        surface.PlaySound(toolsound)
-    elseif movement_state >= 1 and playback.ToolFromToState == 1 then
-        playback.ToolFromToState = 2
-        surface.PlaySound(toolsound)
+local HideToolgun = Ponder.API.NewInstruction("HideToolgun")
+HideToolgun.Length = 0.5
+
+function HideToolgun:First()
+
+end
+
+function HideToolgun:Update(playback)
+    local env = playback.Environment
+    local state = env.ToolgunState
+    state.Alpha = Lerp(playback:GetInstructionProgress(self), 1, 0)
+end
+
+function HideToolgun:Last(playback)
+    playback.Environment.ToolgunState = nil
+end
+
+------------------------------------------------------------------
+-- MoveToolgunTo
+------------------------------------------------------------------
+
+local MoveToolgunTo = Ponder.API.NewInstruction("MoveToolgunTo")
+MoveToolgunTo.Length = 0.5
+MoveToolgunTo.Position = Vector(0, 0, 0)
+
+local function GetToolgunPos(playback, target, pos)
+    if isstring(target) then
+        return playback.Environment:GetNamedModel(target):LocalToWorld(pos)
+    elseif target == nil then
+        return pos
+    end
+end
+
+function MoveToolgunTo:First(playback)
+    local env = playback.Environment
+
+    if self.Length == 0 then
+        env.ToolgunState.Position = GetToolgunPos(playback, self.Target, self.Position)
+        return
     end
 
-    local from = playback.Environment:GetNamedModel(self.From)
-    local to   = playback.Environment:GetNamedModel(self.To)
-
-    playback.ToolFromToAlpha = alpha * 255
-    playback.ToolFromToPos = LerpVector(movement_state, from:GetPos(), to:GetPos())
+    env.ToolgunState.LerpingPosition = true
+    env.ToolgunState.StartPos  = env.ToolgunState.Position
+    env.ToolgunState.Target    = self.Target
+    env.ToolgunState.TargetPos = self.Position
 end
 
-function ToolFromTo:Render3D(playback)
-    playback.ToolFromToPos2D = playback.ToolFromToPos:ToScreen()
+function MoveToolgunTo:Update(playback)
+    local env = playback.Environment
+    local state = env.ToolgunState
+
+    if state.LerpingPosition then
+        local progress = playback:GetInstructionProgress(self)
+        progress = self.Easing and self.Easing(progress) or progress
+        state.Position = LerpVector(progress, state.StartPos, GetToolgunPos(playback, state.Target, state.TargetPos))
+    end
 end
 
-function ToolFromTo:Render2D(playback)
-    local x, y = playback.ToolFromToPos2D.x + 32, playback.ToolFromToPos2D.y + 32
+function MoveToolgunTo:Last(playback)
+    local env = playback.Environment
+    local state = env.ToolgunState
 
+    state.LerpingPosition = false
+end
+
+------------------------------------------------------------------
+-- ClickToolgun
+------------------------------------------------------------------
+
+local ClickToolgun = Ponder.API.NewInstruction("ClickToolgun")
+ClickToolgun.Length = 0.5
+ClickToolgun.Position = Vector(0, 0, 0)
+
+function ClickToolgun:First(playback)
+    playback.Environment.ToolgunState.RenderingBeam = true
+    playback.Environment.ToolgunState.BeamStart = playback.Environment.ToolgunState.Position
+    playback.Environment.ToolgunState.BeamEnd = GetToolgunPos(playback, self.Target, self.Position)
+    surface.PlaySound(toolsound)
+end
+
+function HideToolgun:Update(playback)
+    local env = playback.Environment
+    local state = env.ToolgunState
+    state.Alpha = Lerp(playback:GetInstructionProgress(self), 1, 0)
+end
+
+function HideToolgun:Last(playback)
+    playback.Environment.ToolgunState.RenderingBeam = false
+end
+
+------------------------------------------------------------------
+-- Renderer
+------------------------------------------------------------------
+
+local toolgunRenderer = Ponder.API.NewRenderer("Toolgun")
+
+function toolgunRenderer:Initialize(env)
+    env.ToolgunState = nil
+end
+
+function toolgunRenderer:Render3D(env)
+    if not env.ToolgunState then return end
+    env.ToolgunState.ToolFromToPos2D = env.ToolgunState.Position:ToScreen()
+end
+
+function toolgunRenderer:Render2D(env)
+    if not env.ToolgunState then return end
+    local state = env.ToolgunState
+    local alpha = state.Alpha * 255
+
+    local x, y = state.ToolFromToPos2D.x + 32, state.ToolFromToPos2D.y + 32
     surface.SetMaterial(toolgun96)
-    surface.SetDrawColor(255, 255, 255, playback.ToolFromToAlpha)
+    surface.SetDrawColor(255, 255, 255, alpha)
     surface.DrawTexturedRectRotated(x, y, 96, 96, 0)
 
-    draw.SimpleTextOutlined(self.Tool, "DermaLarge", x, y + 32, Color(255, 255, 255, playback.ToolFromToAlpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, playback.ToolFromToAlpha))
+    draw.SimpleTextOutlined(state.ToolName, "DermaLarge", x, y + 32, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, alpha))
 end
