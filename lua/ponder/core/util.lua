b5c62d7f28cb3a33813ab74683cd635c1d13e594
march <106459595+marchc1@users.noreply.github.com>
2024-12-27 14:29:23 -0800

    Move font definitions to utils.lua

--- lua/ponder/core/util.lua
+++ lua/ponder/core/util.lua
@@ -1,25 +1,26 @@
-surface.CreateFont("Ponder.Title", {
-    font = "Tahoma",
-    extended = false,
-    size = 24,
-    weight = 100,
-    antialias = true
-})
+if CLIENT then
+    surface.CreateFont("Ponder.Title", {
+        font = "Tahoma",
+        extended = false,
+        size = 24,
+        weight = 100,
+        antialias = true
+    })
 
-surface.CreateFont("Ponder.Subtitle", {
-    font = "Tahoma",
-    extended = false,
-    size = 38,
-    weight = 100,
-    antialias = true
-})
+    surface.CreateFont("Ponder.Subtitle", {
+        font = "Tahoma",
+        extended = false,
+        size = 38,
+        weight = 100,
+        antialias = true
+    })
 
-surface.CreateFont("Ponder.Text", {
-    font = "Arial",
-    extended = false,
-    size = 20,
-    weight = 900,
-    antialias = true
-})
-
+    surface.CreateFont("Ponder.Text", {
+        font = "Arial",
+        extended = false,
+        size = 20,
+        weight = 900,
+        antialias = true
+    })
+end
 function Ponder.IsStringNilOrEmpty(s) return s == nil or #string.Trim(s) == 0 end
