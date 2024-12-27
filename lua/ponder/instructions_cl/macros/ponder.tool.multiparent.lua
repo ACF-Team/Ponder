0b35d168b0b38b5e6943f65f7d0bad5985ff73dd
march <106459595+marchc1@users.noreply.github.com>
2024-12-27 15:43:07 -0800

    Add Multi-Parent macro

---
+++ lua/ponder/instructions_cl/macros/ponder.tool.multiparent.lua
@@ -0,0 +1,23 @@
+local MultiParent = Ponder.API.NewInstructionMacro("MultiParent")
+
+function MultiParent:Run(chapter, parameters)
+    local length = parameters.Length or 1
+    local timeForEachClick = length / (#parameters.Children + 1)
+    chapter:AddInstruction("ShowToolgun", {Length = timeForEachClick, Tool = "Multi-Parent"})
+    local tAdd = 0
+    for _, child in ipairs(parameters.Children) do
+        chapter:AddInstruction("MoveToolgunTo", {Time = tAdd, Target = child, Easing = parameters.Easing})
+        tAdd = tAdd + timeForEachClick
+        chapter:AddInstruction("ClickToolgun", {Time = tAdd, Target = child})
+        chapter:AddInstruction("ColorModel", {Time = tAdd, Target = child, Length = 0.1, Color = Color(100, 255, 100, 190)})
+    end
+
+    chapter:AddInstruction("MoveToolgunTo", {Time = tAdd, Target = parameters.Parent, Easing = parameters.Easing})
+    chapter:AddInstruction("ClickToolgun", {Time = tAdd + timeForEachClick, Target = parameters.Parent})
+    for _, child in ipairs(parameters.Children) do
+        chapter:AddInstruction("ColorModel", {Time = tAdd + timeForEachClick, Target = child, Length = 0.1, Color = Color(255, 255, 255, 255)})
+    end
+    chapter:AddInstruction("HideToolgun", {Time = tAdd + timeForEachClick + 0, Length = timeForEachClick})
+
+    return tAdd + timeForEachClick
+end
