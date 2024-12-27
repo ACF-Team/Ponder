b90034eacd2e8d8d6898ad00466345372bc0efc3
march <106459595+marchc1@users.noreply.github.com>
2024-12-27 14:28:26 -0800

    Padding for ShowText vars

--- lua/ponder/instructions_cl/ponder.showtext.lua
+++ lua/ponder/instructions_cl/ponder.showtext.lua
@@ -1,14 +1,14 @@
 local ShowText = Ponder.API.NewInstruction("ShowText")
-ShowText.Name       = ""
-ShowText.Markup     = nil
-ShowText.Dimension  = "3D"
-ShowText.Position   = vector_origin
-ShowText.Icons      = {}
-ShowText.Time = 0
-ShowText.Length     = 0.5
-ShowText.Horizontal = TEXT_ALIGN_LEFT
-ShowText.Vertical = TEXT_ALIGN_TOP
-ShowText.TextAlignment = TEXT_ALIGN_LEFT
+ShowText.Name                     = ""
+ShowText.Markup                   = nil
+ShowText.Dimension                = "3D"
+ShowText.Position                 = vector_origin
+ShowText.Icons                    = {}
+ShowText.Time                     = 0
+ShowText.Length                   = 0.5
+ShowText.Horizontal               = TEXT_ALIGN_LEFT
+ShowText.Vertical                 = TEXT_ALIGN_TOP
+ShowText.TextAlignment            = TEXT_ALIGN_LEFT
 ShowText.PositionRelativeToScreen = false
 
 function ShowText:First(playback)
