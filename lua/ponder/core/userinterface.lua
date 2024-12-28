25f190628c9df5c50cdec754d4ae0359ae62fce0
march <106459595+marchc1@users.noreply.github.com>
2024-12-28 10:37:17 -0800

    Add a UI button for Ponder (will move later?)

---
+++ lua/ponder/core/userinterface.lua
@@ -0,0 +1,13 @@
+hook.Add("PopulateMenuBar", "Ponder.PopulateMenuBar.InsertMenuOption", function(menubar)
+    local b = menubar:Add("DButton")
+    b:SetText("Ponder")
+    b:Dock(LEFT)
+    b:DockMargin(5, 0, 0, 0)
+    b:SetIsMenu(true)
+    b:SetPaintBackground(false)
+    b:SizeToContentsX(16)
+
+    function b:DoClick()
+        Ponder.OpenIndex()
+    end
+end)
