b3648aa8872d89fa2267e351fe5463fd908de2d4
march <106459595+marchc1@users.noreply.github.com>
2025-01-08 17:48:03 -0800

    Sorting by language phrase

--- lua/ponder/api_cl.lua
+++ lua/ponder/api_cl.lua
@@ -79,12 +79,42 @@ function API.RegisterAddonCategory(addon_name, category_name, data)
     data.ID = category_name
 end
 
+local function sortByName(tab)
+    local TableMemberSort = function( a, b, MemberName, bReverse )
+        if not istable(a) then return not bReverse end
+        if not istable(b) then return bReverse end
+
+        local aMemberName = language.GetPhrase(a[MemberName])
+        local bMemberName = language.GetPhrase(b[MemberName])
+
+        if not aMemberName then return not bReverse end
+        if not bMemberName then return bReverse end
+
+        if isstring(aMemberName) then
+            if bReverse then
+                return language.GetPhrase(aMemberName:lower()) < language.GetPhrase(bMemberName:lower())
+            else
+                return language.GetPhrase(aMemberName:lower()) > language.GetPhrase(bMemberName:lower())
+            end
+        end
+
+        if bReverse then
+            return aMemberName < bMemberName
+        else
+            return aMemberName > bMemberName
+        end
+
+    end
+
+    table.sort(tab, function(a, b) return TableMemberSort(a, b, "Name", false) end)
+end
+
 -- Gets all addons
 function API.GetAddonsList()
     local items = {}
     for _, v in pairs(API.RegisteredAddons) do items[#items + 1] = v end
 
-    table.SortByMember(items, "Name", true)
+    sortByName(items)
     return items
 end
 
