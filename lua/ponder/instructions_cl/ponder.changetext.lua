7250880d1283bd4ae1d697b30e186d6559ca9aa5
march <106459595+marchc1@users.noreply.github.com>
2025-01-08 18:00:19 -0800

    Implement ChangeText

---
+++ lua/ponder/instructions_cl/ponder.changetext.lua
@@ -0,0 +1,16 @@
+local ChangeText = Ponder.API.NewInstruction("ChangeText")
+ChangeText.Name                     = ""
+ChangeText.Markup                   = nil
+
+function ChangeText:First(playback)
+    local env = playback.Environment
+    local txt = env:GetNamedText(self.Name)
+
+    if not self.Markup then
+        self.Markup = "<font=DermaLarge>" .. tostring(self.Text) .. "</font>"
+    end
+
+    txt:SetMarkup(self.Markup)
+    print(txt)
+    print(self.Markup)
+end
