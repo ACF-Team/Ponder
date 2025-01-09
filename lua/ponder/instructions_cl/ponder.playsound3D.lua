7274fd357454ebeaf1ea741016c8263e44d8c78f
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-01-08 23:12:31 -0500

    Revise PlaySound3D functionality
    StopSound3D has been removed, and you can now instead provide a Length parameter to PlaySound3D for the same functionality

--- lua/ponder/instructions_cl/ponder.playsound3D.lua
+++ lua/ponder/instructions_cl/ponder.playsound3D.lua
@@ -8,7 +8,22 @@ function PlaySound3D:First(playback)
     local mdl = env:GetNamedModel(self.Target)
     if not IsValid(mdl) then return end
 
+    local ply = LocalPlayer()
+    if not ply.Ponder3DSounds then
+        ply.Ponder3DSounds = {}
+    end
+
     local mdlSound = CreateSound(LocalPlayer(), self.Sound)
-    mdl.Sound = mdlSound
+    self.ActiveSound = mdlSound
     mdlSound:Play()
+end
+
+function PlaySound3D:Last(playback)
+    local env = playback.Environment
+    local mdl = env:GetNamedModel(self.Target)
+    if not IsValid(mdl) then return end
+
+    if self.Length ~= 0 and self.ActiveSound:IsPlaying() then
+        self.ActiveSound:Stop()
+    end
 end
