260b5662b4e6dbb665d02c1348c1dcc0ad3dad47
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-01-09 20:23:50 -0500

    Add toolgun beam/marker rendering

--- lua/ponder/instructions_cl/toolgun/ponder.clicktoolgun.lua
+++ lua/ponder/instructions_cl/toolgun/ponder.clicktoolgun.lua
@@ -3,23 +3,27 @@ local ClickToolgun = Ponder.API.NewInstruction("ClickToolgun")
 
 local function GetToolgunPos(playback, target, pos)
     if isstring(target) then
-        return playback.Environment:GetNamedModel(target):LocalToWorld(pos)
+        return playback.Environment:GetNamedModel(target):LocalToWorld(vector_origin)
     elseif target == nil then
         return pos
     end
 end
 
 ClickToolgun.Length = 0.5
-ClickToolgun.Position = Vector(0, 0, 0)
 
 function ClickToolgun:First(playback)
     local env = playback.Environment
     local state = env.ToolgunState
     if not state then return end
 
+    local toolgunPos = playback.Environment.ToolgunState.Position
+    self.Position = self.Position or toolgunPos
+
     state.RenderingBeam = true
-    state.BeamStart = playback.Environment.ToolgunState.Position
+    state.RenderingMarker = true
+    state.BeamStart = toolgunPos
     state.BeamEnd = GetToolgunPos(playback, self.Target, self.Position)
+    state.BeamLength = (state.BeamStart - state.BeamEnd):Length()
 
     if playback.Seeking then return end
     surface.PlaySound(toolsound)
@@ -30,7 +34,17 @@ function ClickToolgun:Update(playback)
     local state = env.ToolgunState
     if not state then return end
 
-    state.BeamAlpha = Lerp(playback:GetInstructionProgress(self), 1, 0)
+    local curProgress = playback:GetInstructionProgress(self)
+
+    state.MarkerAlpha = Lerp(curProgress, 1, 0) * 255
+
+    if state.BeamAlpha and state.BeamAlpha < 1 then
+        state.RenderingBeam = false
+    else
+        state.BeamNorm = (state.BeamStart - state.BeamEnd) * curProgress * 4
+        state.BeamNormLength = state.BeamNorm:Length()
+        state.BeamAlpha = Lerp(curProgress * 4, 1, 0) * 255
+    end
 end
 
 function ClickToolgun:Last(playback)
@@ -39,4 +53,5 @@ function ClickToolgun:Last(playback)
     if not state then return end
 
     state.RenderingBeam = false
+    state.RenderingMarker = false
 end
--- lua/ponder/instructions_cl/toolgun/ponder.toolgun_renderer.lua
+++ lua/ponder/instructions_cl/toolgun/ponder.toolgun_renderer.lua
@@ -1,4 +1,6 @@
 local toolgun96 = Material("ponder/ui/toolgun_cursor96.png")
+local toolTracer = Material("effects/tool_tracer")
+local selectIndicator = Material("effects/select_dot")
 local toolgunRenderer = Ponder.API.NewRenderer("Toolgun")
 
 function toolgunRenderer:Initialize(env)
@@ -6,19 +8,50 @@ function toolgunRenderer:Initialize(env)
 end
 
 function toolgunRenderer:Render3D(env)
-    if not env.ToolgunState then return end
-    env.ToolgunState.ToolFromToPos2D = env.ToolgunState.Position:ToScreen()
+    local state = env.ToolgunState
+    if not state then return end
+
+    state.ToolFromToPos2D = state.Position:ToScreen()
+
+    -- Beam rendering based on the actual tooltracer effect
+    if state.RenderingBeam then
+        local beamAlpha = state.BeamAlpha
+        local beamLength = state.BeamLength / 128
+        local texCoord = math.Rand(0, 1)
+        local beamColor = Color(255, 255, 255, beamAlpha)
+
+        render.SetMaterial(toolTracer)
+
+        for _ = 1, 3 do
+            render.DrawBeam(state.BeamStart - state.BeamNorm, state.BeamEnd, 8, texCoord, texCoord + state.BeamNormLength / 128, beamColor)
+        end
+
+        render.DrawBeam(state.BeamStart, state.BeamEnd, 8, texCoord, texCoord + beamLength, beamColor)
+    end
+
+    if state.RenderingMarker then
+        state.TargetFromToPos2D = state.BeamEnd:ToScreen()
+    end
 end
 
 function toolgunRenderer:Render2D(env)
-    if not env.ToolgunState then return end
     local state = env.ToolgunState
-    local alpha = state.Alpha * 255
+    if not state then return end
 
+    local alpha = state.Alpha * 255
     local x, y = state.ToolFromToPos2D.x + 32, state.ToolFromToPos2D.y + 32
+
     surface.SetMaterial(toolgun96)
     surface.SetDrawColor(255, 255, 255, alpha)
     surface.DrawTexturedRectRotated(x, y, 96, 96, 0)
 
     draw.SimpleTextOutlined(state.ToolName, "DermaLarge", x, y + 32, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, alpha))
+
+    if state.RenderingMarker then
+        local endX, endY = state.TargetFromToPos2D.x, state.TargetFromToPos2D.y
+
+        surface.SetMaterial(selectIndicator)
+        surface.SetDrawColor(255, 255, 255, state.MarkerAlpha)
+        surface.DrawTexturedRect(endX, endY, 32, 32)
+    end
 end
