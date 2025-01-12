9c096875f7adb7eb52a8278e55e62a92989e43a1
march <106459595+marchc1@users.noreply.github.com>
2025-01-12 14:34:21 -0800

    Flesh out LanguageNotice

--- lua/vgui/ponder_storyboard_icon.lua
+++ lua/vgui/ponder_storyboard_icon.lua
@@ -34,6 +34,9 @@ function PANEL:Paint()
     local animGradient = math.ease.OutCirc(animTime)
     local animXGrad = (1 - math.ease.OutQuart(animTime)) * 64
 
+    COLOR_Title:SetUnpacked(170, 170, 170, animGradient * 255)
+    COLOR_Subtitle:SetUnpacked(245, 245, 245, animGradient * 255)
+
     surface.DrawTexturedRect(padding - animXGrad, padding - 12, 500 * animGradient, 48 + 32, 0)
     surface.DrawTexturedRectRotated(padding - animXGrad - (size / 2), (padding - 12) + 40, size, 80, 180)
 
@@ -41,6 +44,7 @@ function PANEL:Paint()
     draw.SimpleText(language.GetPhrase(self.Storyboard.PlaybackName or self.Storyboard.MenuName), "Ponder.Subtitle", 76 + padding - animXGrad, padding + 16 + 6, COLOR_Subtitle)
 
     self.Icon:SetPos(padding - animXGrad, padding)
+    self.Icon:SetAlpha(animGradient * 255)
     local pX, pY = self:LocalToScreen(0, 0)
     self.Icon:PaintAt(pX + (padding - animXGrad), pY + padding)
     DisableClipping(false)
@@ -57,22 +61,31 @@ DEFINE_BASECLASS("Panel")
 
 local Oxygen_Language = Material("ponder/ui/icon64/oxygen_language.png", "mips smooth")
 
+local COLOR_Text = Color(245, 245, 245)
+
 function PANEL:Init()
     self.Title = ""
     self.Desc  = ""
-    self.StartTime = CurTime()
+    self.StartTime = CurTime() + 0.1
+    self:SetSize(500, 40)
+    self:SetText("")
 end
 
-local padding2 = 0
-function PANEL:Paint()
+function PANEL:Paint(_, h)
+    local animTime = math.Clamp((CurTime() - self.StartTime) / 1, 0, 1)
+    if animTime == 0 then return end
+
+    local padding2 = h / 3
     DisableClipping(true)
 
-    surface.SetDrawColor(0, 0, 0, 255)
+    local hM = (self.Hovered and not self:GetDisabled()) and 1 or 0
+    local hr, hg, hb = 66 * hM, 76 * hM, 90 * hM
+    surface.SetDrawColor(hr, hg, hb, 255)
     surface.SetMaterial(GRADIENT)
 
     local size = 32
 
-    local animTime = math.Clamp((CurTime() - self.StartTime) / 1, 0, 1)
+
     local animGradient = math.ease.OutCirc(animTime)
     local animXGrad = (1 - math.ease.OutQuart(animTime)) * 64
 
@@ -81,10 +94,11 @@ function PANEL:Paint()
     surface.DrawTexturedRect(padding2 - animXGrad, padding2 - 12, 500 * animGradient, ySize, 0)
     surface.DrawTexturedRectRotated(padding2 - animXGrad - (size / 2), (padding2 - 12) + (ySize / 2), size, ySize, 180)
 
-    draw.SimpleText(self.Title, "Ponder.Text", 44 + padding2 - animXGrad, padding2 - 8, COLOR_Subtitle)
-    draw.SimpleText(self.Desc,  "Ponder.Text", 44 + padding2 - animXGrad, padding2 + 8, COLOR_Subtitle)
+    COLOR_Text:SetUnpacked(245, 245, 245, animGradient * 255)
+    draw.SimpleText(self.Title, "Ponder.TitleText", 44 + padding2 - animXGrad, padding2 - 8, COLOR_Text)
+    draw.SimpleText(self.Desc,  "Ponder.DescText", 44 + padding2 - animXGrad, padding2 + 8, COLOR_Text)
 
-    surface.SetDrawColor(255, 255, 255)
+    surface.SetDrawColor(255, 255, 255, animGradient * 255)
     surface.SetMaterial(Oxygen_Language)
     surface.DrawTexturedRect(padding2 - animXGrad, padding2 - 6, 32, 32)
 
@@ -95,22 +109,73 @@ function PANEL:OnRemove()
 
 end
 
-function PANEL:SetTranslationQuality(quality)
+function PANEL:SetContactInfo(contactName, contactURL)
+    self.ContactName = contactName
+    self.ContactURL  = contactURL
+end
+
+function PANEL:InitializeOverallPonderNotice()
+    self.StartTime = CurTime()
+
+    local quality = Ponder.Localization:GetCurrentLanguageTranslationQuality()
     local TranslationQuality = Ponder.Localization.TranslationQuality
+    self.Quality = quality
+    self.ContactURL = "https://github.com/ACF-Team/Ponder"
+
+    if quality == TranslationQuality.Unsupported then
+        self.Title, self.Desc =
+            "Ponder does not support the " .. Ponder.Localization.GetCurrentLangName() .. " language.",
+            "Contact us if you wish to translate Ponder to this language. Clicking this notice will redirect you to our GitHub."
+    elseif quality == TranslationQuality.Shaky then
+        self.Title, self.Desc =
+            "Ponder supports the " .. Ponder.Localization.GetCurrentLangName() .. " language, but the translation may be shaky.",
+            "Contact us if you wish to correct this translation. Clicking this notice will redirect you to our GitHub."
+    elseif quality == TranslationQuality.MachineTranslated then
+        self.Title, self.Desc =
+            "Ponder is using machine-learning translation for the " .. Ponder.Localization.GetCurrentLangName() .. " language.",
+            "Contact us if you wish to human translate Ponder into this language. Clicking this notice will redirect you to our GitHub."
+    end
+
+    function self:DoClick()
+        gui.OpenURL(self.ContactURL)
+    end
+    self:SetCursor("hand")
+
+    function self:Think()
+        self:MoveToFront()
+    end
+end
+
+function PANEL:SetTranslationQuality(quality)
+    self.Quality = quality
+    local TranslationQuality = Ponder.Localization.TranslationQuality
+
+    local contactPart = self.ContactName and (self.ContactName .. " ") or "the creator of the storyboard "
+    local clickWorks  = self.ContactURL and " (Click for Contact URL)" or "."
 
     if quality == TranslationQuality.Unsupported then
         self.Title, self.Desc =
             "This storyboard does not support the " .. Ponder.Localization.GetCurrentLangName() .. " language.",
-            "Contact the creator of the storyboard if you wish to translate this storyboard."
+            "Contact " .. contactPart .. "if you wish to translate this storyboard" .. clickWorks
     elseif quality == TranslationQuality.Shaky then
         self.Title, self.Desc =
             "This storyboard supports the " .. Ponder.Localization.GetCurrentLangName() .. " language, but the translation may be shaky.",
-            "Contact the creator of the storyboard if you wish to correct this storyboard's translation."
+            "Contact " .. contactPart .. "if you wish to correct this storyboard's translation" .. clickWorks
     elseif quality == TranslationQuality.MachineTranslated then
         self.Title, self.Desc =
             "This storyboard used machine learning to support the " .. Ponder.Localization.GetCurrentLangName() .. " language.",
-            "Contact the creator of the storyboard if you wish to translate the storyboard more accurately."
+            "Contact " .. contactPart .. "if you wish to translate the storyboard more accurately" .. clickWorks
+    end
+
+    if self.ContactURL then
+        function self:DoClick()
+            gui.OpenURL(self.ContactURL)
+        end
+        self:SetCursor("hand")
+    else
+        self:SetDisabled(true)
+        self:SetCursor("no")
     end
 end
 
-derma.DefineControl("Ponder.LanguageNotice", "Ponder's language quality notice message", PANEL, "Panel")
+derma.DefineControl("Ponder.LanguageNotice", "Ponder's language quality notice message", PANEL, "DButton")
