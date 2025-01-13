70c1bc411155bc6b76d4876d63fb950294ccf51c
march <106459595+marchc1@users.noreply.github.com>
2025-01-13 14:03:21 -0800

    Add Ponder_OnHalt and Ponder_OnContinue to VGUI

--- lua/ponder/instructions_cl/vgui/ponder.vgui.object.lua
+++ lua/ponder/instructions_cl/vgui/ponder.vgui.object.lua
@@ -4,6 +4,19 @@ function VGUIPanel.Initialize(env, _, vgui_type, vgui_parent)
 
     local panel = vgui.Create(vgui_type, parent)
     panel:SetParent(parent)
+
+    function panel:Ponder_OnHalt()
+        self.__Ponder_RestoreEnabled = self:IsEnabled()
+        self.__Ponder_RestoreVisible = self:IsVisible()
+        self:SetEnabled(false)
+        self:SetVisible(false)
+    end
+
+    function panel:Ponder_OnContinue()
+        self:SetEnabled(self.__Ponder_RestoreEnabled == nil and true or self.__Ponder_RestoreEnabled)
+        self:SetVisible(self.__Ponder_RestoreVisible == nil and true or self.__Ponder_RestoreVisible)
+    end
+
     Ponder.VGUI_Support.ConfigureParentPaint(panel, parent)
     return panel
 end
