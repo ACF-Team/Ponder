cf66c907d8dcd000df8e2dacf1bb5cd3ef58c3e3
march <106459595+marchc1@users.noreply.github.com>
2025-01-13 14:03:05 -0800

    Don't play lengthless PlaySound's >SoundDuration

--- lua/ponder/instructions_cl/sound/ponder.playsound.lua
+++ lua/ponder/instructions_cl/sound/ponder.playsound.lua
@@ -3,6 +3,10 @@ local PlaySound = Ponder.API.NewInstruction("PlaySound")
 PlaySound.Volume = 1
 PlaySound.Pitch = 100
 
+-- SoundDuration seems to be buggy with "mp3 files that don't have constant bitrate", so if
+-- you run into issues with the OnResumed part, provide the sound duration here
+PlaySound.SoundDuration = nil
+
 function PlaySound:First(playback)
     if playback.Seeking and self.Length == 0 then return end
     if not self.Sound then return end
@@ -27,6 +31,12 @@ function PlaySound:OnPaused()
     self.ActiveSound:ChangeVolume(0)
 end
 
-function PlaySound:OnResumed()
+function PlaySound:OnResumed(playback)
+    local length = self.Length
+    local duration = self.SoundDuration or SoundDuration(self.Sound)
+    local time = playback.Time
+
+    if length == 0 and (time - playback:GetInstructionStartTime(self)) > duration then return end
+
     self.ActiveSound:ChangeVolume(self.Volume)
 end
